5c6d2b1da4387c85f8c609091a658bfe6dac1069
Paul Taach <taahpavel@gmail.com>
2023-09-03 09:27:55 +0200

    lesson 34 change

--- src/lesson_34/EmployeeNameIdComparator.java
+++ src/lesson_34/EmployeeNameIdComparator.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 public class EmployeeNameIdComparator implements Comparator<Employee> {
 
     public int compare(Employee employee1, Employee employee2){
-        return employee1.getName().equals(employee2.getName()) ? employee1.getName().compareTo(employee2.getName()): employee1.getId()-employee2.getId();
+
+        return employee1.getName().equals(employee2.getName()) ? employee1.getId()-employee2.getId():employee1.getName().compareTo(employee2.getName()) ;
     }
 }
